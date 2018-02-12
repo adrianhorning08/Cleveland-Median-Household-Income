e3204f082501a74b4d9e3321e8443c1fc1d88d10
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-12 15:27:12 -0800

    got api calls

--- frontend/api_requests.js
+++ frontend/api_requests.js
@@ -1,6 +1,17 @@
-export const fetchData = () => {
+const fetchSportsData = (yr) => {
   return $.ajax({
     method: 'GET',
-    url: 'https://api.sportradar.us/nfl-t1/teams/2016/REG/standings.json?api_key=hjyu8hqymwz2umwz29zrf6pn'
+    url: `https://api.sportradar.us/nfl-t1/teams/${yr}/REG/standings.json?api_key=hjyu8hqymwz2umwz29zrf6pn`
   });
 };
+
+const fetchCensusData = () => {
+  return $.ajax({
+    method: 'GET',
+    url: `http://api.datausa.io/api/?show=geo&sumlevel=state&required=avg_wage`
+  });
+};
+
+// fetchCensusData().then(res => console.log(res));
+
+// census bureau api key bc6b73c6cce45b6fdb1a47f9f4785efbd765faa0
